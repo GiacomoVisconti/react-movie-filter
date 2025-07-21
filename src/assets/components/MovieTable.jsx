b01f1f142a6ee255d89d5e20963a23d3cd972087
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-21 16:59:13 +0200

    insert a fiture to add a new film

--- src/assets/components/MovieTable.jsx
+++ src/assets/components/MovieTable.jsx
@@ -3,58 +3,142 @@ import { useState, useEffect } from 'react'
 
 
 export default function MovieTable(){
+    // Set a mode for create or search movies
+    const [isCreating, setIsCreating] = useState(true)
 
-    const [moviesList, setMovieList] = useState(movies)
-    const [search, setSearch] = useState('')
-    const [selected, setSelected] = useState('')
-    const [filteredMovies, setFilteredMovies] = useState(moviesList)
+    // Set Variables
+
+        // create mode
+        const [newMovieTitle, setNewMovieTitle] = useState('')
+        const [newMovieGenre, setNewMovieGenre] = useState('')
+        const [moviesList, setMovieList] = useState(movies)
+        
+        
+        // search mode
+        const [search, setSearch] = useState('')
+        const [selected, setSelected] = useState('')
+        const [filteredMovies, setFilteredMovies] = useState(moviesList)
+
 
     useEffect(() => {
-        console.log(moviesList);
-        
-        const filtered = moviesList.filter((element) => {
+
+        // IF the 'mode' is Not Creating the user can filter the movies
+        if(!isCreating){
+
             
-            if(element.genre.includes(selected) && element.title.includes(search)){
-                return element
-            }
-        })
-        setFilteredMovies(filtered)
+            
+            const filtered = moviesList.filter((element) => {
+                
+                if(element.genre.includes(selected) && element.title.includes(search)){
+                    return element
+                }
+            })
+            console.log(filtered);
+            
+            // setFilteredMovies(filtered)
+            setMovieList(filtered)
+            
+        }
         
         
-    },[search, selected, moviesList])
+    },[search, selected])
 
     // Event Functions
-    function handleSubmit (e){
-        e.preventDefault()
+    function handleClickSearch(){
+        setIsCreating(false)
     }
 
-    function handleChange(e){
+    function handleSubmit (e){
+        e.preventDefault()
+
+        const newMovieObj = {
+            id:moviesList.length + 1,
+            title: newMovieTitle,
+            genre: newMovieGenre
+        }
+
+        
+        setMovieList([...moviesList, newMovieObj])
+    }
+
+    // Search mode
+    function handleChangeSearch(e){
         setSearch(e.target.value)
     }
 
-    function handleSelect(e){
+    function handleSelectSearch(e){
         setSelected(e.target.value)
     }
+
+    // Create mode
+    function handleClickReset(){
+        setIsCreating(true)
+        
+    }
+
+    function handleChangeAddTitle(e){
+        setNewMovieTitle(e.target.value)
+    }
+
+    function handleChangeAddGenre(e){
+        setNewMovieGenre(e.target.value)
+    }
+    
+
+
 
     return (
 
         <>
-            {/* Movie Filters */}
+            
+            
+            {/* Movie Forms */}
             <div className="container my-4">
-                <h2>Filter Movies</h2>
+                <h2>{isCreating ? 'Add Movie' : 'Filter Movies'}</h2>
                 <hr />
-                <form onSubmit={handleSubmit} className='d-flex gap-3'>
-                    <input onChange={handleChange} type="text" value={search} className='form-control' placeholder='Insert the Movie title'/>
-                    <select onChange={handleSelect} className="form-select">
-                        <option defaultValue>Select a Genre</option>
-                        {moviesList.map(({genre}, index) => {
-                            return (
 
-                                <option key={index} value={genre}>{genre}</option>
-                            )
-                        })}
-                    </select>
-                    <button type="submit" className="btn btn-success">Search</button>
+                {/* Filter Movies */}
+                <form className={isCreating ? 'd-none' : 'd-block'}>
+
+                    {/* Inputs */}
+                    <div className='d-flex gap-3'>
+                        <input onChange={handleChangeSearch} type="text" value={search} className='form-control' placeholder='Insert the Movie title'/>
+                        <select onChange={handleSelectSearch} className="form-select">
+                            <option defaultValue>Select a Genre</option>
+                            {moviesList.map(({genre}, index) => {
+                                return (
+
+                                    <option key={index} value={genre}>{genre}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+
+                    {/* Button */}
+                    <div className="d-flex gap-3 justify-content-center py-3">
+                        <button type="button" onClick={handleClickReset} className="btn btn-warning" >Reset</button>
+                    </div>
+                </form>
+
+                {/* Add Movie */}
+                <form onSubmit={handleSubmit} className={isCreating ? 'd-block' : 'd-none'}>
+                    <div className="d-flex gap-3 ">
+
+                        <input onChange={handleChangeAddTitle} type="text" value={newMovieTitle} className='form-control' placeholder='Insert the Movie title'/>
+                        <select onChange={handleChangeAddGenre} className="form-select">
+                            <option defaultValue>Select a Genre</option>
+                            {moviesList.map(({genre}, index) => {
+                                return (
+
+                                    <option key={index} value={genre}>{genre}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                    <div className="d-flex gap-3 justify-content-center py-3">
+                        <button type="submit" className="btn btn-success">Save</button>
+                        <button type="button" className="btn btn-info" onClick={handleClickSearch}>Search</button>
+                    </div>
                 </form>
             </div>
             {/* Movie Table */}
@@ -72,7 +156,7 @@ export default function MovieTable(){
                             </tr>
                         </thead>
                         <tbody>
-                            {filteredMovies.map(({id, title, genre}, index) => {
+                            {moviesList.map(({id, title, genre}, index) => {
                                 return (
 
                                     <tr key={index}>
